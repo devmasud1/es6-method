7614a318f2287bf0c834ffc2b0012f0e41c42184
devmasud1 <devmahmud.masu1@gmail.com>
2023-08-26 23:47:00 +0600

    arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,12 @@
+//normal function
+function sum(a, b) {
+  return a + b;
+}
+const total = sum(2, 3);
+console.log(total);
+
+
+//arrow function
+const multi = (a, b) => a * b;
+const multiTotal = multi(2, 3);
+console.log(multiTotal);
